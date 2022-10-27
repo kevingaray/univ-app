17c5010475282647d18121b36aca5d00ceddfbba
kevin_garay <kgaray@applaudostudios.dev>
2022-10-27 07:48:04 -0500

    add flash messages and errors

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,6 +13,7 @@ import "jquery"
 $(document).on('turbolinks:load', function() {
   $(".dropdown-trigger").dropdown();
   $('.sidenav').sidenav();
+  $('#fade-out-target').fadeOut(4000);
 })
 
 Rails.start()
