67fd1598e632548abee0c0bdbf4622337abc9845
kevin_garay <kgaray@applaudostudios.dev>
2022-10-27 10:11:42 -0500

    build routes and form

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   root 'courses#index'
   get 'courses/new', to: 'courses#new'
-  
   get 'about', to: 'pages#about'
   resources :students
+  # login
+  get 'login', to: 'logins#new'
+  post 'login', to: 'logins#create'
+  delete 'login', to: 'logins#destroy'
+
 end
