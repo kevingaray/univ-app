02a4b03e42adbf5b6dbfe74385548f5ef9719f9e
kevin_garay <kgaray@applaudostudios.dev>
2022-10-26 19:32:34 -0500

    Course validation

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,3 +1,5 @@
 class Course < ApplicationRecord
-
+  validates :short_name, presence: true, length: { maximum: 5 }
+  validates :name, presence: true, length: {minimum: 5, maximum: 50 }
+  validates :description, presence: true, length: {minimum: 10, maximum: 100 }
 end
